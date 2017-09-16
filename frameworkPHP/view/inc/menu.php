190ff6bb3a35fbf1fd6d6c32c22a469ff33e524b
jorbencas <jorbencas@gmail.com>
2017-09-16 13:39:32 +0200

    Plantilla web amb un formulari que consta de una validacio en php i Javascript

---
+++ frameworkPHP/view/inc/menu.php
@@ -0,0 +1,14 @@
+<body >
+  <!-- Nav -->
+    <nav id="nav">
+      <ul>
+        <li><a href="index.php?module=main">Home</a></li>
+            <li><a href="index.php?module=books">Books</a></li>
+            <li><a href="index.php?module=portfolio">PORTFOLIO</a></li>
+            <li><a href="index.php?module=pricing">PRICING</a></li>
+            <li><a href="index.php?module=contact">CONTACT</a></li>
+      </ul>
+    </nav>
+
+  </div>
+   <!--/.NAVBAR END-->
